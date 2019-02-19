cfbbe2664c9c2d5f4cf850542eb3149ea339b063
Aaron Robotham <aaron.robotham@uwa.edu.au>
2019-02-19 15:49:21 +0800

    Lots of small changes and proper docs.

--- R/ProSpect.R
+++ R/ProSpect.R
@@ -1,39 +1,62 @@
 ProSpectSED=function(SFH, z=0.1, tau_birth=1, tau_screen=0.3, tau_AGN=1, pow_birth = -0.7,
                      pow_screen = -0.7, pow_AGN = -0.7, alpha_SF_birth=1, alpha_SF_screen=3,
                      alpha_SF_AGN=0, AGNlum=0, sparse=5, speclib=NULL, Dale=NULL, AGN=NULL,
-                     filtout=NULL, returnall=TRUE, H0=67.8, OmegaM=0.308, OmegaL=1-OmegaM,
-                     ref, ...){
+                     filtout=NULL, Dale_M2L_func=NULL, returnall=TRUE, H0=67.8,
+                     OmegaM=0.308, OmegaL=1-OmegaM, ref, ...){
   
   tau_birth=.interval(tau_birth,0,10)
   tau_screen=.interval(tau_screen,0,10)
   tau_AGN=.interval(tau_AGN,0,10)
-  pow_birth=.interval(pow_birth,0,10)
-  pow_screen=.interval(pow_screen,0,10)
-  pow_AGN=.interval(pow_AGN,0,10)
-  alpha_SF_birth=.interval(alpha_SF_birth,0,10)
-  alpha_SF_screen=.interval(alpha_SF_screen,0,10)
-  alpha_SF_AGN=.interval(alpha_SF_AGN,0,10)
+  pow_birth=.interval(pow_birth,-2,0)
+  pow_screen=.interval(pow_screen,-2,0)
+  pow_AGN=.interval(pow_AGN,-2,0)
+  alpha_SF_birth=.interval(alpha_SF_birth,0,4)
+  alpha_SF_screen=.interval(alpha_SF_screen,0,4)
+  alpha_SF_AGN=.interval(alpha_SF_AGN,0,4)
 
   Stars=SFH(z=-1, tau_birth=tau_birth, tau_screen=tau_screen, pow_birth=pow_birth, pow_screen=pow_screen, sparse=sparse, speclib=speclib, ...)
   
   Dust_Birth=Dale_interp(alpha_SF=alpha_SF_birth, Dale=Dale)
   Dust_Screen=Dale_interp(alpha_SF=alpha_SF_screen, Dale=Dale)
   
-  SED_Bdust_Sdust=Dust_Screen$Aspec*Stars$lumtot_screen+Dust_Birth$Aspec*Stars$lumtot_birth
-  SED_Stars_Bdust_Sdust=addspec(wave1 = Stars$wave, flux1 = Stars$lum, wave2 = Dust_Screen$Wave, flux2 = SED_Bdust_Sdust, extrap = 0)
+  SED_Bdust_Sdust=Dust_Birth$Aspec*Stars$lumtot_birth+Dust_Screen$Aspec*Stars$lumtot_screen
+  SED_Stars_Bdust_Sdust=addspec(wave1 = Stars$wave_lum, flux1 = Stars$lum_atten, wave2 = Dust_Screen$Wave, flux2 = SED_Bdust_Sdust, extrap = 0)
   
-  StarsAtten=data.frame(wave=Stars$wave, lum=Stars$lum)
-  StarsUnAtten=data.frame(wave=Stars$wave, lum=Stars$lum_unatten)
-  DustEmit=data.frame(wave=Dust_Screen$Wave, lum=SED_Bdust_Sdust)
+  if(!is.null(Dale_M2L_func)){
+    dustlum_birth=Stars$lumtot_birth
+    dustlum_screen=Stars$lumtot_screen
+    dustmass_birth=Stars$lumtot_birth/Dale_M2L_func(alpha_SF_birth)
+    dustmass_screen=Stars$lumtot_screen/Dale_M2L_func(alpha_SF_screen)
+  }else{
+    dustlum_birth=NULL
+    dustlum_screen=NULL
+    dustmass_birth=NULL
+    dustmass_screen=NULL
+  }
+  
   
   if(is.null(AGN) | AGNlum==0){
     Final=SED_Stars_Bdust_Sdust
     AGN=NULL
+    dustlum_AGN=0
+    dustmass_AGN=0
   }else{
     #First we attenuate by the hot taurus
-    AGN=atten_emit(wave=AGN$Wave, flux=AGN$Aspec*AGNlum/(3.828e33), tau=tau_AGN, pow=pow_AGN, alpha_SF=alpha_SF_AGN, Dale=Dale)$final
+    AGN=atten_emit(wave=AGN$Wave, flux=AGN$Aspec*AGNlum/(3.828e33), tau=tau_AGN, pow=pow_AGN, alpha_SF=alpha_SF_AGN, Dale=Dale, Dale_M2L_func=Dale_M2L_func)
+    if(!is.null(Dale_M2L_func) & returnall){
+      dustlum_AGN=AGN$total_atten
+      dustmass_AGN=AGN$dustmass
+    }
     #Second we re-attenuate the above by the screen (since it still has to pass out of the galaxy)
-    AGN=atten_emit(wave=AGN$wave, flux=AGN$flux, tau=tau_screen, pow=pow_screen, alpha_SF=alpha_SF_screen, Dale=Dale)$final
+    AGN=atten_emit(wave=AGN$final$wave, flux=AGN$final$flux, tau=tau_screen, pow=pow_screen, alpha_SF=alpha_SF_screen, Dale=Dale, Dale_M2L_func=Dale_M2L_func)
+    if(!is.null(Dale_M2L_func) & returnall){
+      dustlum_screen=dustlum_screen+AGN$total_atten
+      dustmass_screen=dustmass_screen+AGN$dustmass
+    }else{
+      dustlum_AGN=0
+      dustmass_AGN=0
+    }
+    AGN=AGN$final
     Final=addspec(wave1=SED_Stars_Bdust_Sdust$wave, flux1=SED_Stars_Bdust_Sdust$flux, wave2=AGN$wave, flux2=AGN$flux, extrap=0)
     colnames(AGN)[2]='lum'
   }
@@ -49,14 +72,21 @@ ProSpectSED=function(SFH, z=0.1, tau_birth=1, tau_screen=0.3, tau_AGN=1, pow_bir
   }
   
   if(returnall){
-    return(invisible(list(Photom=photom_out, FinalFlux=Flux, FinalLum=Final, StarsAtten=StarsAtten, StarsUnAtten=StarsUnAtten, DustEmit=DustEmit, AGN=AGN)))
+    StarsAtten=data.frame(wave=Stars$wave_lum, lum=Stars$lum_atten)
+    StarsUnAtten=data.frame(wave=Stars$wave, lum=Stars$lum_unatten)
+    DustEmit=data.frame(wave=Dust_Screen$Wave, lum=SED_Bdust_Sdust)
+    return(invisible(list(Photom=photom_out, FinalFlux=Flux, FinalLum=Final, StarsAtten=StarsAtten, StarsUnAtten=StarsUnAtten, DustEmit=DustEmit, AGN=AGN, Stars=Stars, dustmass=c(birth=dustmass_birth, screen=dustmass_screen, AGN=dustmass_AGN, total=dustmass_birth+dustmass_screen+dustmass_AGN), dustlum=c(birth=dustlum_birth, screen=dustlum_screen, AGN=dustlum_AGN, total=dustlum_birth+dustlum_screen+dustlum_AGN))))
   }else{
     return(invisible(photom_out))
   }
 }
 
 ProSpectSEDlike=function(parm=c(8,9,10,10,0,-0.5,0.2), Data){
+  if(is.null(Data$fit)){Data$fit='optim'}
+  if(is.null(Data$like)){Data$like='st'}
+  if(is.null(Data$verbose)){Data$verbose=TRUE}
   if(Data$verbose){print(parm)}
+  
   if(is.null(Data$logged)){
     parmlist=parm
   }else if(length(Data$logged)==1){
@@ -71,7 +101,7 @@ ProSpectSEDlike=function(parm=c(8,9,10,10,0,-0.5,0.2), Data){
   }
   
   names(parmlist)=Data$parmnames
-  SEDout=do.call('ProSpectSED', args=c(parmlist, list(SFH=Data$SFH), list(speclib=Data$speclib), list(Dale=Data$Dale), list(AGN=Data$AGN), list(filtout=Data$filtout), Data$fixed))
+  SEDout=do.call('ProSpectSED', args=c(parmlist, list(SFH=Data$SFH), list(speclib=Data$speclib), list(Dale=Data$Dale), list(AGN=Data$AGN), list(filtout=Data$filtout), Data$arglist))
   
   cutsig=(Data$flux$flux-SEDout$Photom)/Data$flux$fluxerr
   if(Data$like=='norm'){
--- R/SFH.R
+++ R/SFH.R
@@ -122,7 +122,7 @@ SFHp4=function(burstmass=1e8, youngmass=1e9, oldmass=1e10, ancientmass=1e10, bur
   lumtot_atten=sum(c(0,diff(speclib$Wave))*lum)
   
   if(z<0){
-    return(invisible(list(wave=speclib$Wave, lum=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
+    return(invisible(list(wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
   }
   if(z>0){
     flux=Lum2Flux(wave = speclib$Wave, lum = lum, z = z, H0 = H0, OmegaM = OmegaM, OmegaL = OmegaL, ref = ref)
@@ -167,7 +167,7 @@ SFHp4=function(burstmass=1e8, youngmass=1e9, oldmass=1e10, ancientmass=1e10, bur
   SFR=masses$Forming/ages$duration
   sSFR=SFR/masses$Formed
 
-  invisible(list(flux=flux, out=out, wave=speclib$Wave, lum=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, M2L=masstot/lumtot_unatten, ages=ages, masses=masses, SFR=SFR, sSFR=sSFR))
+  invisible(list(flux=flux, out=out, wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, M2L=masstot/lumtot_unatten, ages=ages, masses=masses, SFR=SFR, sSFR=sSFR))
 }
 
 SMstarp4=function(burstmass=1e8, youngmass=1e9, oldmass=1e10, ancientmass=1e10, burstage=c(0,1e8), youngage=c(1e8,1e9), oldage=c(1e9,9e9), ancientage=c(9e9,1.3e10), stellpop='BC03lr', speclib=NULL, Z=c(5,5,5,5), z=0, H0 = 67.8, OmegaM = 0.308, OmegaL = 1 - OmegaM, ref, cossplit=c(9e9,1.3e10), dosplit=FALSE){
@@ -363,7 +363,7 @@ SFHp5=function(burstmass=1e8, youngmass=1e9, midmass=1e10, oldmass=1e10, ancient
   lumtot_atten=sum(c(0,diff(speclib$Wave))*lum)
   
   if(z<0){
-    return(invisible(list(wave=speclib$Wave, lum=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
+    return(invisible(list(wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
   }
   if(z>0){
     flux=Lum2Flux(wave = speclib$Wave, lum = lum, z = z, H0 = H0, OmegaM = OmegaM, OmegaL = OmegaL, ref = ref)
@@ -408,7 +408,7 @@ SFHp5=function(burstmass=1e8, youngmass=1e9, midmass=1e10, oldmass=1e10, ancient
   SFR=masses$Forming/ages$duration
   sSFR=SFR/masses$Formed
 
-  return=list(flux=flux, out=out, lum=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, M2L=masstot/lumtot_unatten, ages=ages, masses=masses, SFR=SFR, sSFR=sSFR)
+  return=list(flux=flux, out=out, wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, M2L=masstot/lumtot_unatten, ages=ages, masses=masses, SFR=SFR, sSFR=sSFR)
 }
 
 SMstarp5=function(burstmass=1e8, youngmass=1e9, midmass=1e10, oldmass=1e10, ancientmass=1e10, burstage=c(0,1e8), youngage=c(1e8,1e9), midage=c(1e9,5e9), oldage=c(5e9,9e9), ancientage=c(9e9,1.3e10), stellpop='BC03lr', speclib=NULL, Z=c(5,5,5,5,5), z=0, H0 = 67.8, OmegaM = 0.308, OmegaL = 1 - OmegaM, ref, cossplit=c(9e9,1.3e10), dosplit=FALSE){
@@ -592,7 +592,7 @@ SFHfunc=function(massfunc=function(age, SFR=1){ifelse(age<1.3e+10,SFR,0)}, force
   lumtot_atten=sum(c(0,diff(speclib$Wave))*lum)
   
   if(z<0){
-    return(invisible(list(wave=speclib$Wave, lum=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
+    return(invisible(list(wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten,lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen))) # returns the minimal luminosity outputs
   }
   if(z>0){
     flux=Lum2Flux(wave = speclib$Wave, lum = lum, z = z, H0 = H0, OmegaM = OmegaM, OmegaL = OmegaL, ref = ref)
@@ -628,7 +628,7 @@ SFHfunc=function(massfunc=function(age, SFR=1){ifelse(age<1.3e+10,SFR,0)}, force
     }
   }
   
-  return=list(flux=flux, out=out, lum=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, massvec=massvec, masstot=masstot, M2L=masstot/lumtot_unatten)
+  return=list(flux=flux, out=out, wave_lum=speclib$Wave, lum_atten=lum, lum_unatten=lum_unatten, masstot=masstot, lumtot_unatten=lumtot_unatten, lumtot_atten=lumtot_atten, lumtot_birth=lumtot_birth, lumtot_screen=lumtot_screen, massvec=massvec, masstot=masstot, M2L=masstot/lumtot_unatten)
 }
 
 SMstarfunc=function(massfunc=function(age, SFR=1){ifelse(age<1.3e+10,SFR,0)}, forcemass=FALSE, unimax=13.8e9, agescale=1, burstage=c(0,1e8), youngage=c(1e8,1e9), midage=c(1e9,5e9), oldage=c(5e9,9e9), ancientage=c(9e9,1.3e10), stellpop='BC03lr', speclib=NULL, Z=5, z=0.1, H0=67.8, OmegaM=0.308, OmegaL=1-OmegaM, ref, ...){
