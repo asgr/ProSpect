1a96bc76d144d731cce9eadf1c1ccb4842c8a741
Aaron Robotham <aaron.robotham@uwa.edu.au>
2024-03-07 18:14:19 +0800

    spec_download function added for converting FITS format data to the required internal R structure. No Docs yet.

--- R/speclib_download.R
+++ R/speclib_download.R
@@ -9,3 +9,42 @@ speclib_download = function(stellpop = 'avail',  destpath='', URL = 'https://tin
                          )
   }
 }
+
+load_speclib = function(file){
+  if(!requireNamespace("Rfits", quietly = TRUE)){
+    stop('The Rfits package is needed for stacking to work. Please install from GitHub asgr/Rfits.', call. = FALSE)
+  }
+  
+  output = list()
+  temp = Rfits::Rfits_read(file, header=FALSE, pointer=FALSE, data.table=FALSE)
+  output$Z = temp$Z
+  output$Age = temp$Age
+  output$AgeBins = temp$AgeBins
+  output$AgeWeights = temp$AgeWeights
+  output$Wave = temp$Wave
+  
+  output$Labels$Zlab = "Metallicity"
+  output$Labels$Agelab = "Time since ZAM / Yrs"
+  output$Labels$Wavelab = "Wavelength / Ang"
+  output$Labels$Lumlab = "Lsun / Ang (for 1 Msun SF)"
+  output$Labels$LumAgelab = "Lsun / Ang (for 1 Msun/Yr SFR)"
+    
+  Zspec_loc = grep(pattern = 'Zspec', names(temp))
+  Zspec = temp[Zspec_loc]
+  names(Zspec) = NULL
+  output$Zspec = Zspec
+  
+  Zevo_loc = grep(pattern = 'Zevo', names(temp))
+  Zevo = temp[Zevo_loc]
+  names(Zevo) = NULL
+  for(i in 1:length(Zevo)){
+    colnames(Zevo[[i]]) = c('SMstar', 'SMgas', 'SMtot', 'SFR', 'SMrem')
+  }
+  output$Zevo = Zevo
+  
+  return(output)
+}
+
+check_specib = function(speclib){
+  
+}
